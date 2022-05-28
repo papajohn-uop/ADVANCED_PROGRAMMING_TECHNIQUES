66facaa3757978e330e9a81edd05dbcce783d06b
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-05-28 17:22:18 +0300

    Initial memos and getAll endpoint

--- course.py
+++ course.py
@@ -7,6 +7,18 @@ from typing import List
 app = FastAPI()
 
 
+my_memos={
+            "AdvProgCourse":"Do not forget projects" ,
+            "JuneExams":"Study" ,
+            "Vacations":"Make resrvations"
+           }
+
 @app.get("/")
 async def root():
     return "Hello Advanced Programming....."
+
+
+
+@app.get("/getNotes")
+async def getNotes():
+    return my_memos
