6ffd04321030a436f62758020d69cff01efb1cc4
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-05-28 17:50:18 +0300

    Add getNote endpoint

--- course.py
+++ course.py
@@ -43,3 +43,21 @@ async def getNotes():
         tmp_memo=Memo(Title=key,Content=value)
         my_memos_list.append(tmp_memo)
     return my_memos_list
+
+
+@app.get("/getNote/{memo_title}",
+    response_model=Memo, 
+    responses={
+        401: {"model": Error},
+        404: {"model": Error}
+        },
+    tags=["READ"],
+    summary="List a specific memo",
+  )
+async def getNote(memo_title):
+    if memo_title in my_memos:
+        selected_memo=Memo(Title=memo_title,Content=my_memos[memo_title])
+        return selected_memo
+    else:
+        error=Error(code=404,reason="This memo does not exist")
+        return JSONResponse(status_code=404, content={"code": error.code,"reason":error.reason})
