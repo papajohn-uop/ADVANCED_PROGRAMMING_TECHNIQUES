ee3041bfd15228c27e4de31861e6af57c4c43eb2
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-05-28 18:04:57 +0300

    Step 6

---
+++ course_6.py
@@ -0,0 +1,90 @@
+from typing import List
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import List
+
+
+class Memo(BaseModel):
+    Title: str
+    Content: str
+
+class Error(BaseModel):
+    code: int
+    reason: str
+
+
+app = FastAPI()
+
+
+my_memos={
+            "AdvProgCourse":"Do not forget projects" ,
+            "JuneExams":"Study" ,
+            "Vacations":"Make resrvations"
+           }
+
+@app.get("/")
+async def root():
+    return "Hello Advanced Programming....."
+
+
+
+@app.get("/getNotes",
+    response_model=List[Memo], 
+    responses={404: {"model": Error}},
+    tags=["READ"],
+    summary="List current memos",
+
+)
+async def getNotes():
+    my_memos_list=list()
+    for key, value in my_memos.items() :
+        print (key, value)
+        tmp_memo=Memo(Title=key,Content=value)
+        my_memos_list.append(tmp_memo)
+    return my_memos_list
+
+
+@app.get("/getNote/{memo_title}",
+    response_model=Memo, 
+    responses={
+        401: {"model": Error},
+        404: {"model": Error}
+        },
+    tags=["READ"],
+    summary="List a specific memo",
+  )
+async def getNote(memo_title):
+    if memo_title in my_memos:
+        selected_memo=Memo(Title=memo_title,Content=my_memos[memo_title])
+        return selected_memo
+    else:
+        error=Error(code=404,reason="This memo does not exist")
+        return JSONResponse(status_code=404, content={"code": error.code,"reason":error.reason})
+
+
+
+#create a new memo
+@app.post("/createNote",
+    response_model=List[Memo], 
+    responses={
+        401: {"model": Error},
+        422: {"model": Error}
+        },
+    tags=["WRITE"],
+    summary="Create a new memo",
+  )
+async def insertNote(new_memo:Memo):
+    print(new_memo.Title)
+    if new_memo.Title in my_memos:
+        error=Error(code=422,reason="This memo already exists")
+        return JSONResponse(status_code=422, content={"code": error.code,"reason":error.reason})
+    else:
+        #append new memo in memo list
+        my_memos[new_memo.Title]=new_memo.Content
+        my_memos_list=list()
+        for key, value in my_memos.items() :
+            print (key, value)
+            tmp_memo=Memo(Title=key,Content=value)
+            my_memos_list.append(tmp_memo)
+        return my_memos_list
